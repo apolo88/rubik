3c6d1778c960f500a1e4f4bc9d1ecd71c0ba392b
Alejandro Spinelli <alejandrospinelli@Alejandros-MacBook-Pro-2.local>
2021-10-24 00:18:14 -0300

    initial commit

---
+++ code/Rubik.py
@@ -0,0 +1,134 @@
+import sys
+from random import randrange
+
+class Rubik:
+    
+    #Dimension of the Rubik Cube
+    dim = None
+
+    #Properties to store the cube, one matrix per layer and references to pieces
+    layersMap = {}
+    piecesMap = {}
+
+    #Create and fill the rubik
+    def initCube(self):
+        pass
+    
+    #Movement methods
+    def moveFront(self):
+        pass
+
+    def moveFrontP(self):
+        pass
+
+    def moveBack(self):
+        pass
+
+    def moveBackP(self):
+        pass
+    
+    def moveUp(self):
+        pass
+
+    def moveUpP(self):
+        pass
+    
+    def moveDown(self):
+        pass
+        
+    def moveDownP(self):
+        pass
+    
+    def moveLeft(self):
+        pass
+
+    def moveLeftP(self):
+        pass
+
+    def moveRight(self):
+        pass        
+
+    def moveRightP(self):
+        pass
+
+    def fullRotateVertical(self):
+        self.moveUp()
+        self.moveDownP()
+
+    def fullRotateVerticalP(self):
+        self.moveUpP()
+        self.moveDown()
+
+    def fullRotateHorizontal(self):
+        self.moveRight()
+        self.moveLeftP()
+
+    def fullRotateHorizontalP(self):
+        self.moveRightP()
+        self.moveLeft()
+
+    def fullRotateProfund(self):
+        self.moveFront()
+        self.moveBackP()
+
+    def fullRotateProfundP(self):
+        self.moveFrontP()
+        self.moveBack()
+
+
+    #Disorder Method
+    def disorder(self, complexity):
+        
+        numOfMovements = None
+        if complexity == 'Simple':
+            numOfMovements = 10
+        elif complexity == 'Medium':
+            numOfMovements = 25
+        elif complexity == 'Hard':
+            numOfMovements = 50
+
+        for i in range(numOfMovements):
+            randNumber = randrange(13)
+            print(randNumber)
+            if randNumber == 1:
+                self.moveFront()
+            elif randNumber == 2:
+                self.moveFrontP()
+            elif randNumber == 3:
+                self.moveBack()
+            elif randNumber == 4:
+                self.moveBackP()
+            elif randNumber == 5:
+                self.moveUp()
+            elif randNumber == 6:
+                self.moveUpP()
+            elif randNumber == 7:
+                self.moveDown()
+            elif randNumber == 8:
+                self.moveDownP()
+            elif randNumber == 9:
+                self.moveLeft()
+            elif randNumber == 10:
+                self.moveLeftP()
+            elif randNumber == 11:
+                self.moveRight()
+            elif randNumber == 12:
+                self.moveRightP()
+
+
+    #Solve Method
+    def solve(self, mode):
+        print('Solve will come soon')
+        pass
+
+    #Output Methods
+    def printCube(self):
+        self.printCubeFront()
+        print('\n')
+        self.printCubeBack()
+
+    def printCubeFront(self):
+        pass
+    
+    def printCubeBack(self):
+        pass
---
+++ code/script.py
@@ -0,0 +1,161 @@
+from Rubik import *
+from Rubik2x2 import *
+import sys
+
+
+def main():
+    
+    #Initial Menu
+    print('Welcome to Rubik! \n')
+    dimOption = showDimMenu()
+    rubikInstance = None
+
+    #Checks the dimension for the cube
+    if dimOption == '1':
+        print('Rubik 2x2')
+        rubikInstance = Rubik2x2()
+    elif dimOption == '2':
+        print('Rubik 3x3')
+        rubikInstance = Rubik3x3()
+    else:
+        print('Incorrect Option')
+        return
+    
+    print('\n')
+
+    #Creates and initialize the cube
+    rubikInstance.initCube()
+
+    modeOption = None
+    while modeOption != '4':
+        print('\n')
+        modeOption = showModeMenu()
+
+        #Manual
+        if modeOption == '1':
+            manualMode(rubikInstance)
+        #Disorder
+        elif modeOption == '2':
+            disorderMode(rubikInstance)
+        #Solve
+        elif modeOption == '3':
+            solveMode(rubikInstance)
+
+
+##############
+### SHOW MENUS
+##############
+def showMenu(description, menuOptions) -> str:
+    menu = description + ': \n'
+    i = 1
+    for option in menuOptions:
+        menu += str(i) + ' ' + option + '\n'
+        i = i + 1
+
+    return input(menu)
+
+def showDimMenu() -> str:
+    dims = ['2x2','3x3']
+    return showMenu('Please select a dimension', dims)
+
+def showModeMenu() -> str:
+    modes = ['Manual','Disorder','Solve','Cancel']
+    return showMenu('Please select a mode', modes)
+
+def showMoveMenu() -> str:
+    moves = ['F','F\'','B','B\'','U','U\'','D','D\'','L','L\'','R','R\'','Previous Menu']
+    return showMenu('Please select a movement', moves)
+
+def showDisorderMenu() -> str:
+    disOptions = ['Simple','Medium','Hard','Previous Menu']
+    return showMenu('Please select the disorder complexity', disOptions)
+
+def showSolveMenu() -> str:
+    solveOptions = ['Step by Step','Direct','Previous Menu']
+    return showMenu('Please select the solve mode', solveOptions)
+
+################
+### MENU ACTIONS
+################
+def manualMode(rubikInstance) -> str:
+    moveOption = None
+    while moveOption != '13':
+        print('\n')
+        rubikInstance.printCube()
+        sys.stdin.read(1)
+        
+        moveOption = showMoveMenu()
+        # F
+        if moveOption == '1':
+            rubikInstance.moveFront()
+        # F'
+        elif moveOption == '2':
+            rubikInstance.moveFrontP()
+        # B
+        elif moveOption == '3':
+            rubikInstance.moveBack()
+        # B'
+        elif moveOption == '4':
+            rubikInstance.moveBackP()
+        # U
+        elif moveOption == '5':
+            rubikInstance.moveUp()
+        # U'
+        elif moveOption == '6':
+            rubikInstance.moveUpP()
+        # D
+        elif moveOption == '7':
+            rubikInstance.moveDown()
+        # D'
+        elif moveOption == '8':
+            rubikInstance.moveDownP()
+        # L
+        elif moveOption == '9':
+            rubikInstance.moveLeft()
+        # L'
+        elif moveOption == '10':
+            rubikInstance.moveLeftP()
+        # R
+        elif moveOption == '11':
+            rubikInstance.moveRight()
+        # R'
+        elif moveOption == '12':
+            rubikInstance.moveRightP()
+
+
+def disorderMode(rubikInstance) -> str:    
+    print('\n')
+    disorderOption = showDisorderMenu()
+    # Simple
+    if disorderOption == '1':
+        rubikInstance.disorder('Simple')
+    # Medium
+    elif disorderOption == '2':
+        rubikInstance.disorder('Medium')
+    # Hard
+    elif disorderOption == '3':
+        rubikInstance.disorder('Hard')
+    elif disorderOption == '4':
+        return
+
+    rubikInstance.printCube()
+
+
+def solveMode(rubikInstance) -> str:    
+    print('\n')
+    solveOption = showSolveMenu()
+    # Step by Step
+    if solveOption == '1':
+        rubikInstance.solve('Step')
+    # Direct
+    elif solveOption == '2':
+        rubikInstance.solve('Direct')
+    # Previous Menu
+    elif solveOption == '3':
+        return
+
+    #rubikInstance.printCube()
+    
+
+if __name__ == '__main__':
+    main()
